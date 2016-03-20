c0817a00472ec11b9ce0edb71255ccdfaf299b93
Sujoy Das <sujoy.das@digitalinsight.com>
2016-03-20 02:08:43 -0700

    Test rpccall on getting user request

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,27 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-//	res.send('Hello');
+var commands = require("./lib/commands");
+var simpleClient = require("./lib/client");
 
+var connection = {
+	    port: 4784,
+	    host: '127.0.0.1',
+	    user: "test",
+	    pass: "test"
+	};
+
+exports.index = function(req, res){
+//  res.render('index', { title: 'Express' });
+//	res.send('Hello');
+	var client = simpleClient(connection);
+    client.call("getInfo", null, function (err, resp) {
+//        cb(err, res);
+    	if (err) {
+    		console.log("Error in getblock");
+    	} else {
+    		console.log("Getting getblock");
+    		res.json(resp);
+    	}
+    });
 };
