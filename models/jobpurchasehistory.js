5e9c2fec725382ee84582e98c8bf9b3f9018194f
Sujoy Das <sujoy.das@digitalinsight.com>
2016-04-28 15:56:03 -0700

    Initial code modules for showing job history

---
+++ models/jobpurchasehistory.js
@@ -0,0 +1,19 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var jobTransactionSchema = mongoose.Schema({
+	seller			: String,
+	buyer			: String,	
+	title     		: String,
+    description     : String,
+    price      		: Number,
+    date        	: Date
+
+});
+
+// create the model for job transaction history
+module.exports = mongoose.model('Jobtransactionhistory', jobTransactionSchema);
